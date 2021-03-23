60db2a2bfccf631036c33597c78462118f0afcf0
darryl34 <manchesterunitedfc277@gmail.com>
2021-03-23 19:32:37 +0800

    add answer text and textbox

--- practice_exercise.py
+++ practice_exercise.py
@@ -8,6 +8,8 @@ from settings import *
 
 GRIDBOX_H, GRIDBOX_W = 50, 50
 MARGIN = 5
+TEXTBOX_ACTIVE = (137, 209, 254)
+TEXTBOX_MAX_LENGTH = 40
 
 pygame.init()
 window = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
@@ -15,18 +17,24 @@ clock = pygame.time.Clock()
 filePath = ""
 matrixList = []
 
+# init buttons and textbox
 loadButton = pygame.Rect(DISPLAY_W/2-50, DISPLAY_H/2, 100, 50)
 smallLoadButton = pygame.Rect(20, 20, 80, 40)
 prevButton = pygame.Rect(DISPLAY_W/2-210, 40, 120, 40)
 nextButton = pygame.Rect(DISPLAY_W/2+50, 40, 120, 40)
+answerInputTextBox = pygame.Rect(350, 445, 400, 30)
 
+# init fonts
 customFont = pygame.font.SysFont('comicsans', 35)
 smallFont = pygame.font.SysFont('comicsans', 25)
+answerFont = pygame.font.SysFont('Calibri', 17)
 
+# init text with corresponding fonts
 loadText = customFont.render("Load", True, WHITE)
 smallLoadText = smallFont.render("Load", True, WHITE)
 prevText = smallFont.render("Previous", True, WHITE)
 nextText = smallFont.render("Next", True, WHITE)
+answerText = smallFont.render("Answer: ", True, BLACK)
 
 
 def prompt_file():
@@ -45,10 +53,12 @@ def loadData(filePath):
     return questionList
 
 
+# helper function to get rows x cols of matrix
 def getMatrixDimensions(matrix):
     return numpy.array(matrix).shape
 
 
+# display question text on screen
 def drawMatrixQuestion(matrix):
     numFont = pygame.font.SysFont('Arial', 15)
     questionText = matrix.get_text()
@@ -56,10 +66,13 @@ def drawMatrixQuestion(matrix):
     text = numFont.render(questionText, True, BLACK)
     window.blit(text, (DISPLAY_W/2-110, 120))
 
+
+# display matrix data on screen
 def drawMatrix(matrix):
     matrix1, matrix2 = matrix.get_matrices()[0], matrix.get_matrices()[1]
     numFont = pygame.font.SysFont('Arial', 15)
 
+    # display matrix at centre
     if matrix2 is None:
         rows, cols = getMatrixDimensions(matrix1)
         for row in range(rows):
@@ -70,6 +83,7 @@ def drawMatrix(matrix):
                 text = numFont.render(str(matrix1[row][col]), True, BLACK)
                 window.blit(text, (gridX+(GRIDBOX_W/2 - text.get_width()/2), gridY+(GRIDBOX_H/2 - text.get_height()/2)))
 
+    # display both matrices side by side
     else:
         rows1, cols1 = getMatrixDimensions(matrix1)
         rows2, cols2 = getMatrixDimensions(matrix2)
@@ -77,7 +91,7 @@ def drawMatrix(matrix):
         for row in range(rows1):
             for col in range(cols1):
                 gridX = (MARGIN+GRIDBOX_W)*col+MARGIN+DISPLAY_W/4-50
-                gridY = (MARGIN+GRIDBOX_H)*row+MARGIN+200
+                gridY = (MARGIN+GRIDBOX_H)*row+MARGIN+180
                 pygame.draw.rect(window, WHITE, [gridX, gridY,GRIDBOX_W,GRIDBOX_H])
                 text = numFont.render(str(matrix1[row][col]), True, BLACK)
                 window.blit(text, (gridX+(GRIDBOX_W/2 - text.get_width()/2), gridY+(GRIDBOX_H/2 - text.get_height()/2)))
@@ -85,20 +99,30 @@ def drawMatrix(matrix):
         for row in range(rows2):
             for col in range(cols2):
                 gridX = (MARGIN+GRIDBOX_W)*col+MARGIN+3*DISPLAY_W/4-120
-                gridY = (MARGIN+GRIDBOX_H)*row+MARGIN+200
+                gridY = (MARGIN+GRIDBOX_H)*row+MARGIN+180
                 pygame.draw.rect(window, WHITE, [gridX, gridY,GRIDBOX_W,GRIDBOX_H])
-                text = numFont.render(str(matrix1[row][col]), True, BLACK)
+                text = numFont.render(str(matrix2[row][col]), True, BLACK)
                 window.blit(text, (gridX+(GRIDBOX_W/2 - text.get_width()/2), gridY+(GRIDBOX_H/2 - text.get_height()/2)))
 
+# display texts related to answer
+def drawMatrixAnswer(matrix):
+    matrixType = matrix.get_question_type()
+    window.blit(answerText, (250, 450))
+    pygame.draw.rect(window, WHITE, answerInputTextBox)
+
+# display answer textfield
+def drawAnswerText(inputText):
+    displayText = answerFont.render(inputText, True, BLACK)
+    window.blit(displayText, (355, 450))
+    pygame.display.update()
 
 # draw start screen
 def draw_window():
 
-    window.fill(pygame.color.Color("grey"))     # fill background
-    pygame.draw.rect(window, (255, 0, 0), loadButton)   # load button
-    window.blit(loadText, (DISPLAY_W/2-50 + 23, DISPLAY_H/2 + 15))         # load text
-    pygame.display.set_caption("Import matrix file")
-
+    window.fill(pygame.color.Color("grey"))
+    pygame.draw.rect(window, (255, 0, 0), loadButton)
+    window.blit(loadText, (DISPLAY_W/2-50 + 23, DISPLAY_H/2 + 15))
+    pygame.display.set_caption("Load Matrix From File")
     pygame.display.update()
 
 # draw matrix to screen
@@ -114,7 +138,8 @@ def draw_matrix_window(matrix):
 
     drawMatrixQuestion(matrix)
     drawMatrix(matrix)
-    pygame.display.set_caption("Import matrix file")
+    drawMatrixAnswer(matrix)
+    pygame.display.set_caption("Practice Questions")
     pygame.display.flip()
 
 # main window
@@ -122,6 +147,8 @@ def main():
     running = True
     global matrixList
     exerciseIndex = 0
+    inputText = ""
+    inputBoxActive = False
 
     draw_window()
     while running:
@@ -131,27 +158,61 @@ def main():
                 running = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos       # gets mouse position
+                mouse_pos = event.pos       # get mouse position
 
                 # checks if mouse position is over button
                 if loadButton.collidepoint(mouse_pos) or smallLoadButton.collidepoint(mouse_pos):
                     prompt_file()
                     matrixList = loadData(filePath)
                     draw_matrix_window(matrixList[0])
-                if nextButton.collidepoint(mouse_pos):
+                    inputText = ""
+
+                elif nextButton.collidepoint(mouse_pos):
                     try:
                         exerciseIndex += 1
                         draw_matrix_window(matrixList[exerciseIndex])
+                        inputText = ""
                     except IndexError:
                         exerciseIndex -= 1
                         #print("No matrix to read!")
-                if prevButton.collidepoint(mouse_pos) and exerciseIndex > 0:
+
+                elif prevButton.collidepoint(mouse_pos) and exerciseIndex > 0:
                     try:
                         exerciseIndex -= 1
                         draw_matrix_window(matrixList[exerciseIndex])
+                        inputText = ""
                     except IndexError:
                         exerciseIndex += 1
                         #print("No matrix to read!")
+
+                elif answerInputTextBox.collidepoint(mouse_pos):
+                    pygame.draw.rect(window, TEXTBOX_ACTIVE, answerInputTextBox)
+                    inputBoxActive = True
+
+            # if key pressed and input box has been clicked
+            elif event.type == pygame.KEYDOWN and inputBoxActive:
+                if event.key == pygame.K_RETURN:
+                    pygame.draw.rect(window, WHITE, answerInputTextBox)
+                    drawAnswerText(inputText)
+                    inputBoxActive = False
+
+                elif event.key == pygame.K_BACKSPACE:
+                    inputText = inputText[:-1]
+                    if len(inputText) >= TEXTBOX_MAX_LENGTH:
+                        trimmedText = inputText[-TEXTBOX_MAX_LENGTH:]
+                        pygame.draw.rect(window, TEXTBOX_ACTIVE, answerInputTextBox)
+                        drawAnswerText(trimmedText)
+                    else:
+                        pygame.draw.rect(window, TEXTBOX_ACTIVE, answerInputTextBox)
+                        drawAnswerText(inputText)
+
+                else:
+                    inputText += event.unicode
+                    if len(inputText) >= TEXTBOX_MAX_LENGTH:
+                        trimmedText = inputText[-TEXTBOX_MAX_LENGTH:]
+                        pygame.draw.rect(window, TEXTBOX_ACTIVE, answerInputTextBox)
+                        drawAnswerText(trimmedText)
+                    else: drawAnswerText(inputText)
 
 
         pygame.display.update()
