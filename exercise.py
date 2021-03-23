2149e8b7968902a69d2778947601ff63321ab87f
darryl34 <manchesterunitedfc277@gmail.com>
2021-03-23 23:00:28 +0800

    fixed bug when creating new Exercise objects

--- exercise.py
+++ exercise.py
@@ -277,9 +277,8 @@ class Exercise:
         return q
 
 if __name__ == "__main__":
-    e = Exercise()
-    q1 = e.generate_exercise(5)
-    q2 = e.import_exercise("test.txt")
+    q1 = Exercise().generate_exercise(5)
+    q2 = Exercise().import_exercise("test.txt")
     print("==================== Random ======================")
     for x in q1:
         print("Text:        " + str(x.get_text()))
