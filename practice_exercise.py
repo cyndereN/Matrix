9062612698b22164ee175a4bec12d6dcaaf3ade5
darryl34 <manchesterunitedfc277@gmail.com>
2021-03-27 21:28:14 +0800

    fixed exerciseIndex bug

--- practice_exercise.py
+++ practice_exercise.py
@@ -299,6 +299,7 @@ class PracticeExercise:
                         self.matrixList = self.generateData()
                         self.draw_matrix_window(self.matrixList[0])
                         inputText = ""
+                        exerciseIndex = 0
                         answerSubmitted = False
                         mainMenuActive = False
                         practiceScreenActive = True
